752aff5a144b91adc53330a04f61c6802208bad1
Pham Thai Nguyen <thainguyen122004@gmail.com>
2026-02-13 00:00:15 +0700

    fix(security): change requestMatchers of ping api

--- src/main/java/com/example/TaskManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/example/TaskManagementSystem/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                         // Public endpoints (no auth required)
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers("/api/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/ping").permitAll()
+                        .requestMatchers("/api/ping").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/projects").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
